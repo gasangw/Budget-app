adf082317ad14a88a7514f319afa7249a60d8263
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-24 12:56:01 +0200

    Create associations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,5 @@
 class Entity < ApplicationRecord
+  belongs_to :user
+  has_many :entity_groups
+  has_many :groups, through: :entity_groups
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_024_094_041) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_104122) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'entities', force: :cascade do |t|
-    t.string 'name'
-    t.decimal 'amount'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_entities_on_user_id'
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_groups_on_user_id'
+  create_table "entity_groups", force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_entity_groups_on_entity_id"
+    t.index ["group_id"], name: "index_entity_groups_on_group_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  add_foreign_key 'entities', 'users'
-  add_foreign_key 'groups', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "entities", "users"
+  add_foreign_key "entity_groups", "entities"
+  add_foreign_key "entity_groups", "groups"
+  add_foreign_key "groups", "users"
 end
