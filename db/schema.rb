cdd09cfebf43ea295e8fde635022f03fcd2144f3
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-24 11:50:39 +0200

    Add models, controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_083844) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_094041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_083844) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
