4a2fc0d5f35af82961ba38efb587f1287ba5d7ea
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-24 18:06:51 +0200

    Solve linters

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Group, Type: :model do
-    before(:each) do
-        @group = Group.create(name: 'samson', icon: 'https://www.google.com')
+  before(:each) do
+    @group = Group.create(name: 'samson', icon: 'https://www.google.com')
+  end
+
+  context 'It should have valid values' do
+    it 'Is valid when name is not black' do
+      @group.name = ''
+      expect(@group).to_not be_valid
     end
 
-    context 'It should have valid values' do
-        it 'Is valid when name is not black' do
-            @group.name = ''
-            expect(@group).to_not be_valid 
-        end
-
-        it 'its invalid when the icon is not present' do
-            @group.icon = ''
-            expect(@group).to_not be_valid
-        end
+    it 'its invalid when the icon is not present' do
+      @group.icon = ''
+      expect(@group).to_not be_valid
     end
+  end
 end
 
 # describe Group do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, Type: :model do
-    before(:each) do
-        @user = User.create(name: 'Thomas')
-    end
+  before(:each) do
+    @user = User.create(name: 'Thomas')
+  end
 
-    describe 'it should have valid values' do
-        it 'the name should be present' do
-            @user.name = nil
-            expect(@user).to_not be_valid
-        end
+  describe 'it should have valid values' do
+    it 'the name should be present' do
+      @user.name = nil
+      expect(@user).to_not be_valid
     end
+  end
 end
