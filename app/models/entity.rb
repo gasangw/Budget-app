11a5f8133d7709bcea92c04b3b433194caa3a239
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-24 13:01:36 +0200

    Add validations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,7 @@ class Entity < ApplicationRecord
   belongs_to :user
   has_many :entity_groups
   has_many :groups, through: :entity_groups
+
+  validate :name, presence: true, length: { minimum: 3 }
+  validate :amount, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :entity_groups
   has_many :entities, through: :entity_groups
+
+  validate :name, presence: true, length: { minimum: 3 }
+
 end
