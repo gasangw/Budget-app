c418b45fa858cfacbf604a54c9590a916d8deeda
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-10-28 10:27:20 +0200

    Correct linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   # mount_uploader :icon, IconUploader
   belongs_to :user
   has_many :entities, dependent: :destroy
-  
+
   # has_one_attached :image
 
   validates :name, presence: true, length: { minimum: 3 }
